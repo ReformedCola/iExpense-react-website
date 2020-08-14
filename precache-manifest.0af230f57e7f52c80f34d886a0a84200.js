4c46b60efaabf5e7ec75ac9e4fa910b176a4a139
ReformedCola <reformedcola@gmail.com>
2020-08-13 17:40:11 -0700

    deploy

---
+++ precache-manifest.0af230f57e7f52c80f34d886a0a84200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375d0790af7776557e5c35ac27564c25",
+    "url": "/iExpense-react-website/index.html"
+  },
+  {
+    "revision": "195b2f178b56c601e002",
+    "url": "/iExpense-react-website/static/css/main.51a9a723.chunk.css"
+  },
+  {
+    "revision": "5bc3945a6738fc5d52c9",
+    "url": "/iExpense-react-website/static/js/2.4ce0f7c7.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/iExpense-react-website/static/js/2.4ce0f7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195b2f178b56c601e002",
+    "url": "/iExpense-react-website/static/js/main.52a0c774.chunk.js"
+  },
+  {
+    "revision": "2e56290b41e3eb4dc49f",
+    "url": "/iExpense-react-website/static/js/runtime-main.e5af21bc.js"
+  }
+]);
